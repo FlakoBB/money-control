d01bc039bb8dd4c19ddbc9fccb48b005714682a7
FlakoBB <josephrbb99@gmail.com>
2024-10-05 17:28:20 -0600

    initial setup

---
+++ src/app/login/page.jsx
@@ -0,0 +1,13 @@
+import styles from '../../styles/loginForm.module.scss'
+
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h2>Login</h2>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ src/app/page.jsx
@@ -0,0 +1,7 @@
+export default function Home () {
+  return (
+    <main>
+      <h1>MONEY CONTROL</h1>
+    </main>
+  )
+}
